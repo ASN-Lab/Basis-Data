d2e03190b0e18e8cc1432ea26c2d8221165fe9b9
Aldiyan S N <asn.labs1210@gmail.com>
2025-02-28 10:41:22 +0700

    Rename file

--- TugasModul2_DBMS_2420506031.sql
+++ TugasModul2_DBMS_2420506031.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 26, 2025 at 10:17 AM
+-- Generation Time: Feb 17, 2025 at 03:44 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,113 +18,172 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `akademik`
+-- Database: `pengiriman_barang`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dosen`
+-- Table structure for table `barang`
 --
 
-CREATE TABLE `dosen` (
-  `DosenID` int(11) NOT NULL,
-  `Nama` varchar(100) NOT NULL,
-  `Email` varchar(100) NOT NULL,
-  `Telepon` varchar(15) DEFAULT NULL,
-  `Alamat` text DEFAULT NULL,
-  `Gelar` varchar(50) DEFAULT NULL,
-  `TanggalBergabung` date DEFAULT NULL
+CREATE TABLE `barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `deskripsi_barang` text DEFAULT NULL,
+  `berat_barang` decimal(10,2) NOT NULL,
+  `dimensi_barang` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mahasiswa`
+-- Table structure for table `pelanggan`
 --
 
-CREATE TABLE `mahasiswa` (
-  `MahasiswaID` int(11) NOT NULL,
-  `Nama` varchar(100) NOT NULL,
-  `NIM` varchar(20) NOT NULL,
-  `Email` varchar(100) NOT NULL,
-  `Telepon` varchar(15) DEFAULT NULL,
-  `Alamat` text DEFAULT NULL,
-  `TanggalLahir` date DEFAULT NULL,
-  `TanggalMasuk` date DEFAULT NULL
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` int(11) NOT NULL,
+  `nama_pelgan` varchar(100) NOT NULL,
+  `alamat_pelanggan` text NOT NULL,
+  `telepon_pelanggan` varchar(15) NOT NULL,
+  `email_pelanggan` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `matakuliah`
+-- Table structure for table `pembayaran`
 --
 
-CREATE TABLE `matakuliah` (
-  `MataKuliahID` int(11) NOT NULL,
-  `Nama` varchar(100) NOT NULL,
-  `SKS` tinyint(4) NOT NULL,
-  `DosenID` int(11) DEFAULT NULL
-) ;
+CREATE TABLE `pembayaran` (
+  `id_pembayaran` int(11) NOT NULL,
+  `id_pengiriman` int(11) DEFAULT NULL,
+  `metode_pembayaran` varchar(50) NOT NULL,
+  `tanggal_pembayaran` date NOT NULL,
+  `jumlah_pembayaran` decimal(10,2) NOT NULL,
+  `status_pembayaran` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengirim`
+--
+
+CREATE TABLE `pengirim` (
+  `id_pengirim` int(11) NOT NULL,
+  `nama_pengirim` varchar(100) NOT NULL,
+  `alamat_pengirim` text NOT NULL,
+  `telepon_pengirim` varchar(15) NOT NULL,
+  `email_pengirim` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengiriman`
+--
+
+CREATE TABLE `pengiriman` (
+  `id_pengiriman` int(11) NOT NULL,
+  `id_pelanggan` int(11) DEFAULT NULL,
+  `id_pengirim` int(11) DEFAULT NULL,
+  `id_barang` int(11) DEFAULT NULL,
+  `tanggal_kirim` date NOT NULL,
+  `tanggal_terima` date DEFAULT NULL,
+  `status_pengiriman` varchar(50) NOT NULL,
+  `biaya_pengiriman` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `dosen`
+-- Indexes for table `barang`
 --
-ALTER TABLE `dosen`
-  ADD PRIMARY KEY (`DosenID`),
-  ADD UNIQUE KEY `Email` (`Email`);
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`);
 
 --
--- Indexes for table `mahasiswa`
+-- Indexes for table `pelanggan`
 --
-ALTER TABLE `mahasiswa`
-  ADD PRIMARY KEY (`MahasiswaID`),
-  ADD UNIQUE KEY `NIM` (`NIM`),
-  ADD UNIQUE KEY `Email` (`Email`);
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id_pelanggan`);
 
 --
--- Indexes for table `matakuliah`
+-- Indexes for table `pembayaran`
 --
-ALTER TABLE `matakuliah`
-  ADD PRIMARY KEY (`MataKuliahID`),
-  ADD KEY `fk_dosen` (`DosenID`);
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`),
+  ADD KEY `id_pengiriman` (`id_pengiriman`);
+
+--
+-- Indexes for table `pengirim`
+--
+ALTER TABLE `pengirim`
+  ADD PRIMARY KEY (`id_pengirim`);
+
+--
+-- Indexes for table `pengiriman`
+--
+ALTER TABLE `pengiriman`
+  ADD PRIMARY KEY (`id_pengiriman`),
+  ADD KEY `id_pelanggan` (`id_pelanggan`),
+  ADD KEY `id_pengirim` (`id_pengirim`),
+  ADD KEY `id_barang` (`id_barang`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `dosen`
+-- AUTO_INCREMENT for table `barang`
 --
-ALTER TABLE `dosen`
-  MODIFY `DosenID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `mahasiswa`
+-- AUTO_INCREMENT for table `pelanggan`
 --
-ALTER TABLE `mahasiswa`
-  MODIFY `MahasiswaID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `pelanggan`
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `matakuliah`
+-- AUTO_INCREMENT for table `pembayaran`
 --
-ALTER TABLE `matakuliah`
-  MODIFY `MataKuliahID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `pembayaran`
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pengirim`
+--
+ALTER TABLE `pengirim`
+  MODIFY `id_pengirim` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pengiriman`
+--
+ALTER TABLE `pengiriman`
+  MODIFY `id_pengiriman` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `matakuliah`
+-- Constraints for table `pembayaran`
 --
-ALTER TABLE `matakuliah`
-  ADD CONSTRAINT `fk_dosen` FOREIGN KEY (`DosenID`) REFERENCES `dosen` (`DosenID`),
-  ADD CONSTRAINT `matakuliah_ibfk_1` FOREIGN KEY (`DosenID`) REFERENCES `dosen` (`DosenID`);
+ALTER TABLE `pembayaran`
+  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`id_pengiriman`) REFERENCES `pengiriman` (`id_pengiriman`);
+
+--
+-- Constraints for table `pengiriman`
+--
+ALTER TABLE `pengiriman`
+  ADD CONSTRAINT `pengiriman_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`),
+  ADD CONSTRAINT `pengiriman_ibfk_2` FOREIGN KEY (`id_pengirim`) REFERENCES `pengirim` (`id_pengirim`),
+  ADD CONSTRAINT `pengiriman_ibfk_3` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
